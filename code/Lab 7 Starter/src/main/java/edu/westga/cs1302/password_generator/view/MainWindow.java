4d2839267e7788ee96504160c0c7a4f5648957de
Samantha Cook <sc00278@my.westga.edu>
2025-11-06 08:53:40 -0500

    binding for lab 7
    1) binding the components

--- code/Lab 7 Starter/src/main/java/edu/westga/cs1302/password_generator/view/MainWindow.java
+++ code/Lab 7 Starter/src/main/java/edu/westga/cs1302/password_generator/view/MainWindow.java
@@ -1,5 +1,6 @@
 package edu.westga.cs1302.password_generator.view;
 
+import java.text.ParseException;
 import java.util.Random;
 
 import edu.westga.cs1302.password_generator.model.PasswordGenerator;
@@ -31,40 +32,41 @@ public class MainWindow {
     private MainWindowViewModel viewModel;
     private PasswordGenerator generator;
 
-    @FXML
-    void generatePassword(ActionEvent event) {
-//    	int minimumLength = -1;
+//    @FXML
+//    void generatePassword(ActionEvent event) {
+////    	int minimumLength = -1;
+////    	
+////    	try {
+////    		minimumLength = Integer.parseInt(this.minimumLength.getText());
+////    	} catch (NumberFormatException numberError) {
+////    		Alert alert = new Alert(AlertType.ERROR);
+////    		alert.setContentText("Invalid Minimum Length: must be a positive integer, but was " + this.minimumLength.getText());
+////    		alert.show();
+////    		return;
+////    	}
+////    	
+////    	try {
+////    		this.generator.setMinimumLength(minimumLength);
+////    	} catch (IllegalArgumentException invalidLengthError) {
+////    		Alert alert = new Alert(AlertType.ERROR);
+////    		alert.setContentText("Invalid Minimum Length: " + invalidLengthError.getMessage());
+////    		alert.show();
+////    		return;
+////    	}
+////    	
+////    	this.generator.setMustHaveAtLeastOneDigit(this.mustIncludeDigits.isSelected());
+////    	this.generator.setMustHaveAtLeastOneLowerCaseLetter(this.mustIncludeLowerCaseLetters.isSelected());
+////    	this.generator.setMustHaveAtLeastOneUpperCaseLetter(this.mustIncludeUpperCaseLetters.isSelected());
+////    	
+////    	String password = this.generator.generatePassword();
+////    	
+////    	this.output.setText(password);
 //    	
-//    	try {
-//    		minimumLength = Integer.parseInt(this.minimumLength.getText());
-//    	} catch (NumberFormatException numberError) {
-//    		Alert alert = new Alert(AlertType.ERROR);
-//    		alert.setContentText("Invalid Minimum Length: must be a positive integer, but was " + this.minimumLength.getText());
-//    		alert.show();
-//    		return;
-//    	}
+//        this.generatorButton.setOnAction((eve) -> {
+//    		this.viewModel.makePassword();
+//    	});
 //    	
-//    	try {
-//    		this.generator.setMinimumLength(minimumLength);
-//    	} catch (IllegalArgumentException invalidLengthError) {
-//    		Alert alert = new Alert(AlertType.ERROR);
-//    		alert.setContentText("Invalid Minimum Length: " + invalidLengthError.getMessage());
-//    		alert.show();
-//    		return;
-//    	}
-//    	
-//    	this.generator.setMustHaveAtLeastOneDigit(this.mustIncludeDigits.isSelected());
-//    	this.generator.setMustHaveAtLeastOneLowerCaseLetter(this.mustIncludeLowerCaseLetters.isSelected());
-//    	this.generator.setMustHaveAtLeastOneUpperCaseLetter(this.mustIncludeUpperCaseLetters.isSelected());
-//    	
-//    	String password = this.generator.generatePassword();
-//    	
-//    	this.output.setText(password);
-    	
-    	this.generatorButton.setOnAction((eve) -> {
-    		this.viewModel.makePassword();
-    	});
-    }
+//    }  
 
     @FXML
     void initialize() {
@@ -74,19 +76,41 @@ public class MainWindow {
         assert this.minimumLength != null : "fx:id=\"minimumLength\" was not injected: check your FXML file 'MainWindow.fxml'.";
         assert this.output != null : "fx:id=\"output\" was not injected: check your FXML file 'MainWindow.fxml'.";
 
+        
+        this.viewModel = new MainWindowViewModel();
+        this.bindComponents();
         this.minimumLength.setText("1");
         Random randomNumberGenerator = new Random();
         this.generator = new PasswordGenerator(randomNumberGenerator.nextLong());
+        
+        this.generatorButton.setOnAction((eve) -> {
+    		try {
+    			this.viewModel.makePassword();
+    		}
+    		catch (IllegalArgumentException invalidLengthError) {
+    			Alert alert = new Alert(AlertType.ERROR);
+    			alert.setContentText("Invalid Minimum Length: " + invalidLengthError.getMessage());
+    			alert.show();
+    			return;
+    		}
+    		catch (ParseException e) {
+    			Alert alert = new Alert(AlertType.ERROR);
+    			alert.setContentText("Invalid Minimum Length: must be a positive integer, but was " + this.minimumLength.getText());
+    			alert.show();
+    			return;
+    		}
+    	});
     }
     
-//    /**binds the components to the viewModel.
-//     * 
-//     */
-//    public void bindComponents() {
-//    	this.minimumLength.textProperty().bindBidirectional(this.viewModel.minLength(), new NumberStringConverter());
-//    	this.output.textProperty().bind(this.viewModel.getPassword());
-//    	this.mustIncludeDigits.selectedProperty().bindBidirectional(this.viewModel.hasNum());
-//    	this.mustIncludeLowerCaseLetters.selectedProperty().bindBidirectional(this.viewModel.hasLowerCase());
-//    	this.mustIncludeUpperCaseLetters.selectedProperty().bindBidirectional(this.viewModel.hasUpperCase());
-//    }
+    /**binds the components to the viewModel.
+     * 
+     */
+    public void bindComponents() {
+    	this.minimumLength.textProperty().bindBidirectional(this.viewModel.minLength(), new NumberStringConverter());
+    	this.output.textProperty().bindBidirectional(this.viewModel.getPassword());
+    	this.mustIncludeDigits.selectedProperty().bindBidirectional(this.viewModel.hasNum());
+    	this.mustIncludeLowerCaseLetters.selectedProperty().bindBidirectional(this.viewModel.hasLowerCase());
+    	this.mustIncludeUpperCaseLetters.selectedProperty().bindBidirectional(this.viewModel.hasUpperCase());
+    }
+    
 }
