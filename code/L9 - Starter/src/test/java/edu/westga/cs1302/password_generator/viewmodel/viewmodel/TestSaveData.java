01da34a18e55fdb6d739b949380cc0a972f3901f
Samantha Cook <sc00278@my.westga.edu>
2025-11-20 08:51:26 -0500

    last minute fixes

--- code/L9 - Starter/src/test/java/edu/westga/cs1302/password_generator/viewmodel/viewmodel/TestSaveData.java
+++ code/L9 - Starter/src/test/java/edu/westga/cs1302/password_generator/viewmodel/viewmodel/TestSaveData.java
@@ -2,6 +2,10 @@ package edu.westga.cs1302.password_generator.viewmodel.viewmodel;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 import org.junit.jupiter.api.Test;
 
 import edu.westga.cs1302.password_generator.viewmodel.ViewModel;
@@ -16,7 +20,16 @@ class TestSaveData {
 	}
 	
 	@Test
-	void testWhenHasAFile() {
+	void testWhenHasAFile() throws IOException {
+		ViewModel vm = new ViewModel();
+		
+		vm.generatePassword();
+		File save = new File("save.txt");
+		boolean actual = vm.saveData(save);
+		
+		try (Scanner reader = new Scanner(save)) {
+			  assertTrue(actual);
+		  }
 		
 	}
 	
