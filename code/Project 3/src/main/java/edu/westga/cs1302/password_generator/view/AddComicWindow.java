a4fb69b04185c42a0b9cdef2ee5a2380a07391bb
Samantha Cook <sc00278@my.westga.edu>
2025-12-07 20:43:47 -0500

    Project3p2A: Adding Comic to a Collection
    1) updated the MainWindow class to have a listView for displaying comics in a collectin and an add and remove button for the comics in the selected collection.
    2) added a second fxml and codebehind class for adding a comic. the window has a textFeild for the comic title and one for the issue number, a confirm button and a cancel button.

---
+++ code/Project 3/src/main/java/edu/westga/cs1302/password_generator/view/AddComicWindow.java
@@ -0,0 +1,19 @@
+package edu.westga.cs1302.password_generator.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+/**CodeBehind for adding a Comic to a Collection.
+ *
+ * @author samco
+ * @version 1.0 
+ */
+public class AddComicWindow {
+
+	 @FXML private Button addButton;
+	 @FXML private Button cancelButton;
+	 @FXML private TextField comicName;
+	 @FXML private TextField issueNum;
+	
+}
--- code/Project 3/src/main/java/edu/westga/cs1302/password_generator/view/MainWindow.java
+++ code/Project 3/src/main/java/edu/westga/cs1302/password_generator/view/MainWindow.java
@@ -9,6 +9,7 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ContextMenu;
 import javafx.scene.control.ListView;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Alert.AlertType;
 
@@ -25,6 +26,11 @@ public class MainWindow {
     @FXML private ContextMenu contextMenu;
     @FXML private Button removeButton;
     @FXML private Button removeButtonButton;
+    
+    @FXML
+    private MenuItem removeComic;
+    @FXML
+    private ListView<?> comicList;
 
     private ViewModel vm;
     //private ObjectProperty<ComicCollection> selectedCollection;
